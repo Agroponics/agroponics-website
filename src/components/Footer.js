938b60422d134c0045876d266874d366095f45f0
EdanSteen <edansteen@gmail.com>
2025-02-03 11:29:41 -0800

    Fixed the copyright in the footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -8,44 +8,47 @@ function Footer() {
   return (
     <>
       <footer className={styles.footer} id="contact">
-      <div className={styles.contact}>
-            <h4 id='contactTitle'>Contact Us</h4> 
-            <span>
-              <a href='https://maps.app.goo.gl/6H68TqspcrcNMRzp7' target='_blank'>2613 West Mall,<br/>Vancouver, BC, V6T 1Z2</a>
-            </span>
-            <span>
-              <a href="mailto:agroponicsubc@gmail.com" rel="noreferrer">agroponicsubc@gmail.com </a>
-            </span>
+        <div className={styles.footerCols}>
+          <div className={styles.contact}>
+                <h4 id='contactTitle'>Contact Us</h4> 
+                <span>
+                  <a href='https://maps.app.goo.gl/6H68TqspcrcNMRzp7' target='_blank'>2613 West Mall,<br/>Vancouver, BC, V6T 1Z2</a>
+                </span>
+                <span>
+                  <a href="mailto:agroponicsubc@gmail.com" rel="noreferrer">agroponicsubc@gmail.com </a>
+                </span>
+          </div>
+          <div>
+              <h4>Follow Us</h4>
+                <span>
+                  <a href="https://github.com/Agroponics" target="_blank" rel="noreferrer">GitHub</a>
+                </span>
+                <span>
+                  <a href="https://www.instagram.com/ubcagroponics/" target="_blank" rel="noreferrer">Instagram</a>
+                </span>
+                <span>
+                  <a href='https://ca.linkedin.com/company/ubcagrobot' target='_blank' rel='noreferrer'>LinkedIn</a>
+                </span>
+          </div>
+          <div>
+                <h4>Quick Links</h4>
+                <span>
+                  <a onClick={() => router.push('/join')}>Join</a>
+                </span>
+                <span onClick={() => router.push('/about')}>
+                  <a>About</a>
+                </span>
+                <span onClick={() => router.push('/projects')}>
+                  <a>Projects</a>
+                </span>
+                <span onClick={() => router.push('/sponsor')}>
+                  <a>Sponsor</a>
+                </span>
+          </div>
         </div>
-        <div>
-          <h4>Follow Us</h4>
-            <span>
-              <a href="https://github.com/Agroponics" target="_blank" rel="noreferrer">GitHub</a>
-            </span>
-            <span>
-              <a href="https://www.instagram.com/ubcagroponics/" target="_blank" rel="noreferrer">Instagram</a>
-            </span>
-            <span>
-              <a href='https://ca.linkedin.com/company/ubcagrobot' target='_blank' rel='noreferrer'>LinkedIn</a>
-            </span>
-        </div>
-        <div>
-            <h4>Quick Links</h4>
-            <span>
-              <a onClick={() => router.push('/join')}>Join</a>
-            </span>
-            <span onClick={() => router.push('/about')}>
-              <a>About</a>
-            </span>
-            <span onClick={() => router.push('/projects')}>
-              <a>Projects</a>
-            </span>
-            <span onClick={() => router.push('/sponsor')}>
-              <a>Sponsor</a>
-            </span>
-        </div>
+        <span className={styles.copyright}>©2025 Agroponics UBC</span>
       </footer>
-      <span className={styles.copyright}>©2024 Agroponics UBC</span>
+      
     </>
   );
 }
--- src/pages/dog.js
+++ src/pages/dog.js
@@ -1,6 +1,5 @@
 import React from "react";
 import Head from "next/head";
-import Navbar from "../components/Navbar";
 import styles from "../styles/Error.module.css";
 import Image from "next/image";
 import { useRouter } from 'next/router';
@@ -15,11 +14,14 @@ export default function Dog() {
                 <title>DOG</title>
             </Head>
             <main>
-                <h1>Dog has found you</h1>
-                <Image src={`/dog.jpg`} width={800} height={600}></Image>
-                <button className={styles.goHome}>
-                    <a onClick={() => router.push('/')}>Click to return home</a>
-                </button>
+                <div className={styles.dog}>
+                    <h1>Dog has found you</h1>
+                    <Image src={`/dog.jpg`} width={800} height={600}></Image>
+                    <button className={styles.goHome}>
+                        <a onClick={() => router.push('/')}>Click to return home</a>
+                    </button>
+                </div>
+                <Footer />
             </main>
         </>
     );
